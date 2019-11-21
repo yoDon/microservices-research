e4d80e9f64afad7d3fe99821a18f83f902b339c1
Don Alvarez <don@acceleratedpictures.com>
2019-11-20 18:16:04 -0800

    Add Swagger documentation to DTO

--- nestjs/src/demo/dto/create-demo.dto.ts
+++ nestjs/src/demo/dto/create-demo.dto.ts
@@ -1,14 +1,18 @@
 import { IsString } from "class-validator";
 
+import { ApiModelProperty } from "@nestjs/swagger";
+
 interface ICreateDemoDto {
     readonly one: string;
     readonly two: string;
 }
 
 class CreateDemoDto implements ICreateDemoDto {
+    @ApiModelProperty({ type: String })
     @IsString()
     public readonly one: string;
 
+    @ApiModelProperty({ type: String })
     @IsString()
     public readonly two: string;
 }
