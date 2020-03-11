225207ccdf08937363f5e02baa9726a786b246da
Don Alvarez <alvarezdon@outlook.com>
2020-03-10 20:55:18 -0700

    remove unused nestjs boilerplate from demo-api

--- srvs/demo-api/src/app/app.module.ts
+++ srvs/demo-api/src/app/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from "@nestjs/common"; // eslint-disable-line no-unused-vars
 
 import { DemoModule } from "../imports/yodon-demo-api";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 
 @Module({
     imports: [DemoModule],
-    controllers: [AppController],
-    providers: [AppService],
 })
 class AppModule {}
 
